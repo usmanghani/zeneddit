73b58b8ec473e4af6676c48460cc68bf2a0ad094
Abhijeet Dwivedi <ad@Abhijeets-MacBook-Pro.local>
2015-10-01 21:47:04 -0700

    Hackathon

--- main.py
+++ main.py
@@ -1,24 +1,5 @@
-#!/usr/bin/env python
-#
-# Copyright 2008 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
-"""Overheard: A social quote recording site.
-
-Overheard allows users to add quotes and vote on them, with 
-the most popular rising to the top of the rankings.
+"""Zeneddit
 
 Demonstrates:
    * Paging   - both using fetch() and by a unique index
